45ddeb0cff3eca2b5c425ad666661dd821b9bf79
dabinder93 <david.binder1993@gmail.com>
2016-04-27 18:26:33 +0200

    some changes

--- Binder_Schicklgruber_Andorid/app/src/main/java/com/binderschicklgruber/myapplication/MainActivity.java
+++ Binder_Schicklgruber_Andorid/app/src/main/java/com/binderschicklgruber/myapplication/MainActivity.java
@@ -9,9 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
     }
 
     public void thisIsANewMethod(){
-
+		System.out.println("Hello Schicki");
     }
 }
