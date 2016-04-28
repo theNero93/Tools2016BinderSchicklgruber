93b2136b79a76f4db9ad121bc5e698930b59f9ed
dabinder93 <david.binder1993@gmail.com>
2016-04-28 18:17:12 +0200

    bugfix from outter space

---
+++ Tools2016GITKlinarKronlachner/klinar_kronlachner_java/src/klinar_kronlachner_java/Mainclass.java
@@ -0,0 +1,39 @@
+package klinar_kronlachner_java;
+
+public class Mainclass {
+	
+	public static void main (String [] args){
+		
+			System.out("hello git");
+			int j = 0;
+			for (j; j<10; j++){
+				
+			}
+			int i = 0;
+		
+	}
+	
+	
+
+
+
+
+	public void thisIsANewMethod()
+	{
+
+		int [] i = new int [1, 2, 3];
+
+		System.out.println("test");
+
+	}
+	
+	public void bugfixMethod(){
+	
+		System.out.println("bug fixed");
+	}
+	
+	public void soMuchCodeFix(){
+		System.out.println("so many fix");
+		System.out.println("dat wowNess");
+	}
+}
